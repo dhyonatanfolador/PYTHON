52a39a24889ff27421d011fd2c97843bcf6fdc53
dhyonatanfolador <dhyonatan.folador@gmail.com>
2025-07-25 21:21:41 -0300

    Realizado a configuração inicial das URL's

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
 from django.contrib import admin
 from django.urls import path
+from primeiro_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path('sobre/', views.sobre),
 ]
