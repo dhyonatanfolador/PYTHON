bfef2967f431cafc50a4086bd5d12eb94608836a
dhyonatan folador <dhyonatan.folador@gmail.com>
2025-08-07 21:54:15 -0300

    Outras mudanças para melhor performace

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from turtle import home
 from django.contrib import admin
 from django.urls import path
 from primeiro_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    path('about/', views.about),
+    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("about/", views.about),
 ]
