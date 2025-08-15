ea180160fa4a3f3354f539166dd72b9d722e2f72
dhyonatan folador <dhyonatan.folador@gmail.com>
2025-08-14 21:31:12 -0300

    Aprimorando a URL do Django

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from turtle import home
 from django.contrib import admin
 from django.urls import path
 from primeiro_app import views
